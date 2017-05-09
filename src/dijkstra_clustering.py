Fixed bug in plane calculation Added 3d visualization of planes

src/dijkstra_clustering.py
@@ -5,6 +5,15 @@ from mpl_toolkits.mplot3d import Axes3D
 import dijkstra
 
 # https://gist.github.com/amroamroamro/1db8d69b4b65e8bc66a6
+
+def calc_plane(xyz):
+    # solve: z = ax + by + c (plane)
+    # np.c_ slice objects to concatenation along the second axis.
+    A = np.c_[xyz[:, 0], xyz[:, 1], np.ones(xyz.shape[0])]
+    # C = [a, b, c]
+    C, residuals, _, _ = np.linalg.lstsq(A, xyz[:, 2])  # coefficients (a, b, c)
+    return C, residuals
+
 def build_graph(x, data, debug=False):
     assert len(x) == data.shape[0], "Length of x must be equal to number of points in data"
     n_points = data.shape[0]
@@ -17,11 +26,8 @@ def build_graph(x, data, debug=False):
     # Takes 3 points so that a plane could be computed at a later stage
     for i in range(n_points-5):
         for j in range(i+5, n_points):
-            # solve: z = ax + by + c (plane)
-            # np.c_ slice objects to concatenation along the second axis.
-            A = np.c_[x[i:j], data[i:j, 0], np.ones(j-i)]
-            # C = [a, b, c]
-            C, residuals, _, _ = np.linalg.lstsq(A, data[i:j, 1])  # coefficients (a, b, c)
+            xyz = np.c_[x[i:j], data[i:j]]
+            C, residuals = calc_plane(xyz)
 
             if debug:
                 # evaluate it on grid
@@ -105,7 +111,15 @@ def draw_3d_division(x, y, z, path):
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
-    #ax.plot(x[path], y[path], z[path], '.-', color='red')
+
+    for n in range(len(path)-1):
+        xyz = np.c_[x[path[n]:path[n+1]], y[path[n]:path[n+1]], z[path[n]:path[n+1]]]
+        C, residuals = calc_plane(xyz)
+        print(C)
+        xx, yy = np.meshgrid(np.linspace(x[path[n]] - 0.1, x[path[n+1]] + 0.1, 10),
+                             np.linspace(y[path[n]] - 0.1, y[path[n + 1]] + 0.1, 10),)
+        zz = C[0] * xx + C[1] * yy + C[2]
+        ax.plot_surface(xx, yy, zz, rstride=1, cstride=1, alpha=0.5, color='red')
 
     plt.show()
 
@@ -118,8 +132,8 @@ if __name__ == "__main__":
     r3 = np.random.random(15)*0.7
     r4 = np.random.random(35)*0.4*np.linspace(0, 1, 35)
     r = np.concatenate((r1, r2, r3, r4))
-    g = r.copy()
-    #g = np.random.random((n, 100)) * 0.1
+    # g = r.copy()
+    g = np.random.random((n, 100)) * 0.3 + 0.2
     b = np.random.random((n, 100)) * 0.1 + 0.3
     # data - synthetic data representing rgb values of 100 vertices
     data = np.vstack((r, g)).T